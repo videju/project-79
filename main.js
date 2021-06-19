d290019a349fcc09cd2006e97679052b39e4306d
videju <77975858+videju@users.noreply.github.com>
2021-06-19 21:57:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ for(var i=0; i<menu_list_array.length;i++){
 
 
     htmldata=htmldata+'<div class="card">';
-            +'<img src="images\pizza.png"/>'
+            +'<img src="pizza.png"/>'
             +menu_list_array[i] + '<div>'
 }
  htmldata=htmldata+"</section>"
