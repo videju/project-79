eb21abef15f643064c8ccb3e5a82a7aa3c7f61c9
videju <77975858+videju@users.noreply.github.com>
2021-10-07 14:34:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ menu_list_array = ["Veg Margherita Pizza",
 
 function getmenu(){
 var htmldata;
-htmldata="<ol class='menulist'>"
-menu_list_Array.sort();
+htmldata="<ol class='menulist'>";
+menu_list_array.sort();
 for(var i=0;i<menu_list_array.length;i++){
 htmldata=htmldata+'<li>'+ menu_list_array[i] + '</li>'
 }
@@ -21,27 +21,23 @@ document.getElementById("display_menu").innerHTML = htmldata;
 }
 
 function add_item(){
-var htmldata;
-var item=document.getElementById("add_item").value;
-
-menu_list_array.sort();
-htmldata="<section class='cards'>"
-for(var i=0; i<menu_list_array.length;i++){
-
-
-    htmldata=htmldata+'<div class="card">';
-            +'<img src="pizza.png"/>'
-            +menu_list_array[i] + '<div>'
-}
- htmldata=htmldata+"</section>"
-document.getElementById("display_addedmenu").innerHTML = htmldata;
-}
-
-function add_top(){
+var newItem;
 var item=document.getElementById("add_item").value;
 menu_list_array.push(item);
-add_item();
+menu_list_array.sort();
+var newItem ="<section class='cards'>"
+for(var j=0; j<menu_list_array.length;j++){
+
+
+    var newItem =newItem +"<div class='card'>";
+            +"<img src='pizzaImg.png'/>"
+            +menu_list_array[j] + "<div>"
 }
+newItem= newItem+"</section>"
+document.getElementById("display_addedmenu").innerHTML = newItem;
+
+}
+
 
 
  
